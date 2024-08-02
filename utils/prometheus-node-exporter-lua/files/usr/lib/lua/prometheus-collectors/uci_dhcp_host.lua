1a110e5a19327895b0e95dba3c9cab5f098f350c
absentwallaby <64674944+absentwallaby@users.noreply.github.com>
2024-08-03 06:46:40 +1000

    Update uci_dhcp_host.lua to support table entries for MAC
    repairs "string expected, got table" error for mac in previous file
    
    Signed-off-by: absentwallaby <64674944+absentwallaby@users.noreply.github.com>

--- utils/prometheus-node-exporter-lua/files/usr/lib/lua/prometheus-collectors/uci_dhcp_host.lua
+++ utils/prometheus-node-exporter-lua/files/usr/lib/lua/prometheus-collectors/uci_dhcp_host.lua
@@ -6,7 +6,15 @@ local function scrape()
 
   curs:foreach("dhcp", "host", function(s)
     if s[".type"] == "host" then
-      labels = {name=s["name"], mac=string.upper(s["mac"]), dns=s["dns"], ip=s["ip"]}
+    if s[".type"] == "host" then
+      local mac = s["mac"]
+      if type(mac) == "table" then
+        mac = string.upper(mac[1])  -- Take the first element if it's a table
+      end
+      if type(mac) == "string" then
+        mac = string.upper(mac)
+      end
+      local labels = {name = s["name"], mac = mac, dns = s["dns"], ip = s["ip"]}
       metric_uci_host(labels, 1)
     end
   end)
