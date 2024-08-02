8fdd1a7877124d4feac471359a26354cda021b77
absentwallaby <64674944+absentwallaby@users.noreply.github.com>
2024-08-03 06:41:17 +1000

    Update uci_dhcp_host.lua - to get it working again
    previous version would fail to scrape dhcp hosts, with the error "string expected, got table"
    
    Signed-off-by: absentwallaby <64674944+absentwallaby@users.noreply.github.com>

--- utils/prometheus-node-exporter-lua/files/usr/lib/lua/prometheus-collectors/uci_dhcp_host.lua
+++ utils/prometheus-node-exporter-lua/files/usr/lib/lua/prometheus-collectors/uci_dhcp_host.lua
@@ -1,15 +1,20 @@
-local uci=require("uci")
+local uci = require("uci")
 
 local function scrape()
-  local curs=uci.cursor()
+  local curs = uci.cursor()
   local metric_uci_host = metric("uci_dhcp_host", "gauge")
 
   curs:foreach("dhcp", "host", function(s)
     if s[".type"] == "host" then
-      labels = {name=s["name"], mac=string.upper(s["mac"]), dns=s["dns"], ip=s["ip"]}
+      local mac = s["mac"]
+      if type(mac) == "table" then
+        mac = string.upper(mac[1])  -- Take the first element if it's a table
+      end
+      if type(mac) == "string" then
+        mac = string.upper(mac)
+      end
+      local labels = {name = s["name"], mac = mac, dns = s["dns"], ip = s["ip"]}
       metric_uci_host(labels, 1)
     end
   end)
 end
-
-return { scrape = scrape }
